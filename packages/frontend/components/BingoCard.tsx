0df9cfb469c0200a48cadeb252345148392875c4
Oytun Çoban <oytuncoban55@gmail.com>
2022-12-29 16:13:43 +0300

    Description comment for BingoCard

--- packages/frontend/components/BingoCard.tsx
+++ packages/frontend/components/BingoCard.tsx
@@ -2,6 +2,13 @@ import React from "react";
 
 type BingoCardProps = {};
 
+/* 
+  TODO: BingoCard component
+  We will use this component to display cards 
+  for the bingo game. Everything related to bingo card
+  will be in this component. This includes the numbers 
+  hidden or visible, the bingo card itself. 
+*/
 function BingoCard(props: BingoCardProps) {
   return <div>BingoCard</div>;
 }
