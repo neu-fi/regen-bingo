d54cece645ed8b5723a01b418630ef9aec9ed005
tahos81 <tahirozpala@gmail.com>
2022-12-29 15:48:43 +0300

    fixes

--- packages/backend/contracts/RegenBingo.sol
+++ packages/backend/contracts/RegenBingo.sol
@@ -7,13 +7,12 @@ contract RegenBingo is ERC721 {
     uint256 public mintPrice;
     uint256 public drawTimestamp;
     uint256 public drawNumberCooldownSeconds;
-    address public charityAddress;
+    address payable public charityAddress;
 
     uint256[] public drawnNumbers;
-    uint256 lastDrawTime;
-    bool public won = false;
+    uint256 public lastDrawTime;
+    uint256 public totalSupply;
 
-    mapping(uint256 => uint256[25]) public cardNumbers;
     mapping(uint256 => uint256) private _seeds;
 
     constructor(
@@ -22,7 +21,7 @@ contract RegenBingo is ERC721 {
         uint256 _mintPrice,
         uint256 _drawTimestamp,
         uint256 _drawNumberCooldownSeconds,
-        address _charityAddress
+        address payable _charityAddress
     ) ERC721(_name, _symbol) {
         mintPrice = _mintPrice;
         drawTimestamp = _drawTimestamp;
@@ -30,13 +29,14 @@ contract RegenBingo is ERC721 {
         charityAddress = _charityAddress;
     }
 
-    function buyCard(uint256 id) external payable {
-        require(msg.value == CARD_PRICE, "INVALID_PRICE");
-        _mint(msg.sender, id);
+    function mint() external payable {
+        require(msg.value == mintPrice, "INVALID_PRICE");
+        _mint(msg.sender, totalSupply);
+        totalSupply++;
     }
 
     function drawNumber() external returns (uint256) {
-        require(block.timestamp > lastDrawTime + 5 minutes, "DRAW_TOO_SOON");
+        require(block.timestamp > lastDrawTime + drawNumberCooldownSeconds, "DRAW_TOO_SOON");
         uint256 number = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 99;
         drawnNumbers.push(number);
         lastDrawTime = block.timestamp;
@@ -49,10 +49,8 @@ contract RegenBingo is ERC721 {
     }
 
     function claimWinningTicket(uint256 id) external {
-        require(ownerOf(id) == msg.sender, "NOT_OWNER");
-        require(!won, "ALREADY_WON");
         //TODO: check if card has winning combination
-        won = true;
-        payable(msg.sender).transfer(address(this).balance / 2);
+        charityAddress.call{value: address(this).balance / 2}("");
+        payable(ownerOf(id)).call{value: address(this).balance}("");
     }
 }
