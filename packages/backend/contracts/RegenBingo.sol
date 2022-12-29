0814142430b74862edc6e922fbf1be343bb3d975
tahos81 <tahirozpala@gmail.com>
2022-12-29 15:37:56 +0300

    constructor arguments

--- packages/backend/contracts/RegenBingo.sol
+++ packages/backend/contracts/RegenBingo.sol
@@ -1,18 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "packages/backend/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract RegenBingo is ERC721 {
-    uint256 public constant CARD_PRICE = 0.1 ether;
+    uint256 public mintPrice;
+    uint256 public drawTimestamp;
+    uint256 public drawNumberCooldownSeconds;
+    address public charityAddress;
 
-    mapping(uint256 => uint256[25]) public cardNumbers;
-    mapping(uint256 => uint256) private _seeds;
     uint256[] public drawnNumbers;
     uint256 lastDrawTime;
     bool public won = false;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    mapping(uint256 => uint256[25]) public cardNumbers;
+    mapping(uint256 => uint256) private _seeds;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _mintPrice,
+        uint256 _drawTimestamp,
+        uint256 _drawNumberCooldownSeconds,
+        address _charityAddress
+    ) ERC721(_name, _symbol) {
+        mintPrice = _mintPrice;
+        drawTimestamp = _drawTimestamp;
+        drawNumberCooldownSeconds = _drawNumberCooldownSeconds;
+        charityAddress = _charityAddress;
+    }
 
     function buyCard(uint256 id) external payable {
         require(msg.value == CARD_PRICE, "INVALID_PRICE");
@@ -38,9 +54,5 @@ contract RegenBingo is ERC721 {
         //TODO: check if card has winning combination
         won = true;
         payable(msg.sender).transfer(address(this).balance / 2);
-    }
-
-    function withdrawDonationMoney(address withdrawAddress) external {
-        payable(withdrawAddress).transfer(address(this).balance);
     }
 }
