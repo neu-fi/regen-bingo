0fc76ffe66a391d774f829fdb46aebdf7692f84a
tahos81 <tahirozpala@gmail.com>
2022-12-30 16:35:35 +0300

    deployment test

--- packages/backend/test/bingo.ts
+++ packages/backend/test/bingo.ts
@@ -24,8 +24,19 @@ describe("RegenBingo", function () {
         );
         await bingo.deployed();
 
-        return { bingo, addr1, addr2, charity };
+        return { bingo, addr1, addr2, charity, drawTimestamp };
     }
 
-    describe("Deployment", function () {});
+    describe("Deployment", function () {
+        it("Should set constructor arguments correctly", async function () {
+            const { bingo, charity, drawTimestamp } = await loadFixture(deployBingoFixture);
+
+            expect(await bingo.name()).to.equal("RegenBingo");
+            expect(await bingo.symbol()).to.equal("BINGO");
+            expect(await bingo.mintPrice()).to.equal(ethers.utils.parseEther("0.1"));
+            expect(await bingo.drawTimestamp()).to.equal(drawTimestamp);
+            expect(await bingo.drawNumberCooldownSeconds()).to.equal(60 * 5);
+            expect(await bingo.charityAddress()).to.equal(charity.address);
+        });
+    });
 });
