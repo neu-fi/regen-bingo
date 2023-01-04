93bd3b4d8cbd1990c4a24d7943891005f81b9ec1
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2023-01-05 01:14:16 +0300

    useBingoContract automated according to hardhat

--- packages/frontend/hooks/useBingoContract.ts
+++ packages/frontend/hooks/useBingoContract.ts
@@ -1,16 +1,36 @@
-import { CONTRACT_ADDRESS } from "../config"
+import { CONTRACT_ADDRESS, NETWORK_ID, network } from "../config"
 import { regenBingoABI } from "../contracts/regen_bingo_abi"
 import { useContract } from "wagmi"
+import contracts from '@/contracts/hardhat_contracts.json';
 
 export const useBingoContract = (providerOrSigner : any) => {
+   
+    let contractAddress;
+    let contractABI;
+
+    if(network === "hardhat"){
+        const chainId = Number(NETWORK_ID);
+        const allContracts = contracts as any;
+        const hardhatABI = allContracts[chainId][0].contracts.RegenBingo.abi;
+        const hardhatAddress = allContracts[chainId][0].contracts.RegenBingo.address;
+        contractAddress = hardhatAddress;
+        contractABI = hardhatABI;
+    }
+    else{
+        contractAddress = CONTRACT_ADDRESS;
+        contractABI = regenBingoABI;
+    }
+
     const contract = useContract({
-        address: CONTRACT_ADDRESS,
-        abi: regenBingoABI,
+        address: contractAddress,
+        abi: contractABI,
         signerOrProvider: providerOrSigner,
     })
+
     if(!contract){
-        console.error("Contract cannot found")
-        return
+        console.error("Contract cannot found");
+        return;
     }
+
     return contract;
 }
