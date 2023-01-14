a78360d2631489c4a5ca43b1558efd5e12321014
Han Tuzun <hantuzun@users.noreply.github.com>
2023-01-14 12:08:57 +0300

    Log tokenURI in JSON

--- packages/backend/deploy/01_mint_and_log_a_card.ts
+++ packages/backend/deploy/01_mint_and_log_a_card.ts
@@ -6,21 +6,27 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const signer = signers[0];
   const greeterContract = await hre.ethers.getContract("RegenBingo", signer);
   
-  console.log("\ntotalSupply:");
+  console.log("\ntotalSupply():");
   console.log(await greeterContract.totalSupply());
   
-  console.log("\nminting...");
+  console.log("\nMinting...");
   await greeterContract.mint({ value: hre.ethers.utils.parseEther("0.1") })
   
-  console.log("\ntotalSupply:");
+  console.log("\ntotalSupply():");
   console.log(await greeterContract.totalSupply());
-  
-  console.log("\ntokenId:");
+
   let tokenId = await greeterContract.tokenByIndex(0);
+
+  console.log("\ntokenByIndex(0):");
   console.log(tokenId);
   
-  console.log("\ntokenURI:");
-  console.log(await greeterContract.tokenURI(tokenId));
+  let tokenURI = await greeterContract.tokenURI(tokenId);
+
+  console.log("\ntokenURI(tokenId):");
+  console.log(tokenURI);
+
+  console.log("\nDecoded tokenURI(tokenId):");
+  console.log(Buffer.from(tokenURI, 'base64'));
 };
 
 export default main;
