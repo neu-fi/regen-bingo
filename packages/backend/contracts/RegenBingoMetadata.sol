f38757bbfb1c4e017a1587502e1098234dc90afc
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2023-01-18 18:41:54 +0300

    metadata generator contracts

---
+++ packages/backend/contracts/RegenBingoMetadata.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+interface RegenBingoSVG {
+    function generateTokenSVG(
+        uint256[9][3] calldata numbers,
+        bool[9][3] calldata covered
+    ) external view returns (string memory);
+}
+
+contract RegenBingoMetadata {
+    RegenBingoSVG svgGenerator;
+
+    constructor(address _svgGeneratorAddress) {
+        svgGenerator = RegenBingoSVG(_svgGeneratorAddress);
+    }
+
+    function generateTokenURI(
+        uint256 tokenId,
+        uint256[9][3] calldata numbers,
+        bool[9][3] calldata covered
+    ) external view virtual returns (string memory) {
+        string memory svg = svgGenerator.generateTokenSVG(numbers, covered);
+        string memory image = string(
+            abi.encodePacked(
+                "data:image/svg+xml;base64,",
+                Base64.encode(bytes(svg))
+            )
+        );
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        "RegenBingo",
+                        " #",
+                        Strings.toString(tokenId),
+                        '", "description": "...", "image": "',
+                        image,
+                        '"}'
+                    )
+                )
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    function generateContractURI() external pure returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        abi.encodePacked(
+                            '{"name":"RegenBingo"'
+                            '","description":"RegenBingo is a simple experimental game to raise ETH for public goods funding while entertaining us greenpilled regens.","image":"'
+                            ""
+                            '","external_url":"https://www.regen.bingo"}'
+                        )
+                    )
+                )
+            );
+    }
+}
