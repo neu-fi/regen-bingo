78738229c5c382a7e04d71088d345bbd0cb0fdb1
Han Tuzun <hantuzun@users.noreply.github.com>
2023-01-20 16:18:14 +0300

    Deploy to Goerli

--- packages/backend/config.ts
+++ packages/backend/config.ts
@@ -5,7 +5,7 @@ import { parseEther } from 'ethers/lib/utils';
 dotenv.config({ path: '.env' });
 const network = process.env.NEXT_PUBLIC_NETWORK || 'localhost';
 
-const drawTimestamp = Math.floor(Date.now() / 1000) + 120;
+const drawTimestamp = process.env.DRAW_TIMESTAMP || Math.floor(Date.now() / 1000) + 120;
 
 const ethereumArgs = ['REGENBINGO', 'Regen Bingo — #0', parseEther('0.1'), drawTimestamp, 120, '0xc4c302f0d81F3E9a86d468f585dAe26F92D9F5EA'];
 const goerliArgs = ['REGENBINGO', 'Regen Bingo — Testnet #0', parseEther('0.01'), drawTimestamp, 5, '0xc4c302f0d81F3E9a86d468f585dAe26F92D9F5EA'];
