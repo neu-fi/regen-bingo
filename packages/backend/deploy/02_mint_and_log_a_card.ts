a1328fb311f250152a6383ab3131313f18fd8126
koybasimuhittin <koybasimuhittin@gmail.com>
2023-01-23 23:31:35 +0300

    LINK funding after deployment

--- packages/backend/deploy/02_mint_and_log_a_card.ts
+++ packages/backend/deploy/02_mint_and_log_a_card.ts
@@ -1,11 +1,15 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+import { LinkAddress } from '../config';
+import  LinkTokenInstance from "../artifacts/@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol/LinkTokenInterface.json"
 
 const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (hre.network.name !== 'ethereum' ) {
     const signers = await hre.ethers.getSigners();
     const signer = signers[0];
     const regenBingoContract = await hre.ethers.getContract("RegenBingo", signer);
+    const linkTokenABI = LinkTokenInstance["abi"];
+    const linkContract = new hre.ethers.Contract(LinkAddress, linkTokenABI, signer);
     const mintPrice = await regenBingoContract.mintPrice();
     
     console.log("\ntotalSupply():");
@@ -34,6 +38,16 @@ const main: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
     console.log("\nDecoded image:");
     console.log(decodedImage);
+    
+    //302951757588516228 link amount for one random
+    try{
+      let transaction = await linkContract.transfer(regenBingoContract.address, hre.ethers.BigNumber.from(String(40 * 1e16)));
+      transaction.wait();
+      console.log("\n", 40 * 1e16, "LINK sent for funding regen-bingo contract");
+    }catch(err){
+      console.error(`Cannot funded please send LINK to RegenBingo at: ${regenBingoContract.address}`)
+    }
+    
   }
 };
 
