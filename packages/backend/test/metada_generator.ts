870ee0ee2cc23f62c04b689f5e96515c5d7e0e17
koybasimuhittin <koybasimuhittin@gmail.com>
2023-01-23 16:28:36 +0300

    tests

--- packages/backend/test/metada_generator.ts
+++ packages/backend/test/metada_generator.ts
@@ -6,8 +6,11 @@ describe("RegenBingoMetadata", function () {
 
   async function deployRegenBingoMetadataFixture() {
 
+    const DateTime = await ethers.getContractFactory("contracts/DateTimeContract.sol:DateTimeContract");
+    const dateTime = await DateTime.deploy();
+
     const RegenBingoSVG = await ethers.getContractFactory("contracts/RegenBingoSVG.sol:RegenBingoSVG");
-    const regenBingoSVG = await RegenBingoSVG.deploy();
+    const regenBingoSVG = await RegenBingoSVG.deploy(dateTime.address);
 
     const RegenBingoMetadata = await ethers.getContractFactory("contracts/RegenBingoMetadata.sol:RegenBingoMetadata");
     const regenBingoMetadata = await RegenBingoMetadata.deploy(regenBingoSVG.address);
@@ -36,8 +39,13 @@ describe("RegenBingoMetadata", function () {
         const numbers = [[1,2,3,0,0,0,0,0,0],[0,0,0,1,2,3,0,0,0],[0,0,0,0,0,0,1,2,3]];
         const covered = [[1,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,0],[0,0,0,0,0,0,0,0,1]];
         const tokenId = 1;
-  
-        const uri = await regenBingoMetadata.generateTokenURI(tokenId, numbers, covered);
+        const donationAmount = 100;
+        const donationName = "The Gitcoin Alpha Round";
+        const donationAddress = regenBingoMetadata.address;
+        const isBingoFinished = true;
+        const drawTimestamp = Math.floor(Date.now() / 1000);
+
+        const uri = await regenBingoMetadata.generateTokenURI(tokenId, numbers, covered, donationAmount, donationName, donationAddress, isBingoFinished, drawTimestamp);
         console.log("Token uri is: ", uri);
       })
     });
