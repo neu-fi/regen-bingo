b15c41ff4bdb057a78b4b5024d181ce9843abf5e
Oytun Çoban <oytuncoban55@gmail.com>
2023-01-31 18:56:28 +0300

    Example card

--- packages/frontend/components/Hero.tsx
+++ packages/frontend/components/Hero.tsx
@@ -19,29 +19,34 @@ export default function Mint(props: PropsWithChildren<MintProps>) {
   return isMobile ? (
     // Mobile View
     <>
-      <div className="pb-4 bg-green-1 h-[calc(80vh)] flex flex-col justify-between">
-        <div className="flex flex-row justify-center">
-          <div className="h-50vw"> 
-            <div className="bg-img-left object-top bg-no-repeat bg-cover min-h-[25vw] max-h-[45vw] w-2/12 max-w-md"></div>
-          </div>
+      <div className="pb-4 bg-green-1 h-[calc(80vh)] flex flex-col justify-evenly overflow-hidden">
+        <div className="bg-img-left object-top bg-no-repeat bg-cover min-h-[75vw] w-3/4 max-w-md">
+          <img
+            className="transform rotate-12 translate-x-36 translate-y-10"
+            src="./example-board@3x.png"
+            alt="board"
+          />
         </div>
+
         <div className="flex flew-row">
           <div className="flex justify-center items-center">
             <div className="flex flex-col justify-center">
               <div className="flex flex-row justify-center">
-                <span className="font-hero w-1/2">
+                <span className="text-xl font-hero w-2/3">
                   Mint Regen Bingo Cards to contribute to public goods funding
                   while getting a chance to claim the prize pool!
                 </span>
               </div>
-              <div className="flex flex-row justify-center py-2">
+              <div className="flex flex-row justify-center mt-2 py-2">
                 {account.isConnected ? (
                   <>
                     <Link
                       href="/mint"
-                      className="disabled:opacity-50 disabled:cursor-not-allowed inline-flex items-center w-auto rounded-full border-connect-green border-[0.0675rem] bg-green-2 px-4 py-1.5 text-base text-white font-semibold leading-7 shadow-sm hover:bg-green-1"
+                      className="disabled:opacity-50 disabled:cursor-not-allowed inline-flex items-center w-auto rounded-full border-connect-green border-[0.0675rem] bg-green-2 px-24 py-3 text-base text-white font-semibold leading-7 shadow-sm hover:bg-green-1"
                     >
-                      <span>Mint Now!</span>
+                      <span className="text-xl font-hero font-normal">
+                        Mint Now!
+                      </span>
                     </Link>
                   </>
                 ) : (
